b0b885b2d996eda0a25a36af4c546271c1fdc4be
Izere Moubarak <izeremubarak05@gmail.com>
2025-12-15 10:21:01 +0200

    modified providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\ScheduleServiceProvider::class,
+    Webklex\IMAP\Providers\LaravelServiceProvider::class,
 ];
